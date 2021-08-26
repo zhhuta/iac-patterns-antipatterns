71f9b3d46a12b3c01b5e0d618e3bf472ff33b0d9
Vitaliy Zhhuta <zhhuta@gmail.com>
2021-08-26 10:56:25 +0300

    blabla blabla

--- path_tofile/file0.tf
+++ path_tofile/file0.tf
@@ -10,21 +10,3 @@ module "blabla" {
     filter                 = "resource.type = gce_instance AND severity >= WARN"
     unique_writer_identity = true
 }
-
-data "google_billing_account" "account" {
-  billing_account = "000000-0000000-0000000-000000"
-}
-
-resource "google_billing_budget" "budget" {
-  billing_account = data.google_billing_account.account.id
-  display_name = "Example Billing Budget"
-  amount {
-    specified_amount {
-      currency_code = "USD"
-      units = "100000"
-    }
-  }
-  threshold_rules {
-      threshold_percent =  0.5
-  }
-}
