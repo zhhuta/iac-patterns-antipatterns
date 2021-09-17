f39e32cebf96fdc4e11ef325db42f67ac1db64f2
Vitaliy Zhhuta <zhhuta@gmail.com>
2021-09-17 17:22:06 +0300

    boom boom

--- path/sql.tf
+++ path/sql.tf
@@ -1,1 +1,23 @@
-Variables validation error: ('module: cloudsql', ValidationError({'ha_zones': ['Not a valid list.']}))
+module "zhhuta" {
+  source = "git::https://github.com/pcln/terraform-gcp-bucket.git//modules/bucket_advanced_tfo"
+
+  # bucket
+  name = "zhhuta"
+  team = "fly-supply"
+  
+  objectViewer = [
+    "zhhuta@gmail.com",
+  ]
+
+  # passthrough
+  project = var.project
+
+  lifecycle_rules = [{
+    action = {
+      type = "true"
+    }
+    condition = {
+      age = 30
+    }
+  }]
+}
