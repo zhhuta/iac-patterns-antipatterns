04f454d6fafbd2043b4bc95a9fc250b3b144e994
Vitaliy Zhhuta <zhhuta@gmail.com>
2021-09-17 19:48:07 +0300

    boom boom

--- path/sql.tf
+++ path/sql.tf
@@ -1,25 +1,79 @@
+locals {
+
+  database_flags = [
+    { name = "local_infile", value = "off" },
+    { name = "log_bin_trust_function_creators", value = "on" },
+  ]
+  disk_size = 50
+}
 
 module "zhhuta" {
-  source = "git::https://github.com/pcln/terraform-gcp-bucket.git//modules/bucket_advanced_tfo"
 
-  # bucket
-  name = "zhhuta"
-  team = "fly-supply"
-  
-  objectViewer = [
-    "zhhuta@gmail.com",
+
+  source = "git::https://github.com/pcln/terraform-gcp-cloudsql.git//modules/mysql"
+
+  name                = "zhhuta-master"
+  database_version    = "MYSQL_8_0"
+  team                = "data_eng"
+  deletion_protection = true
+
+  settings = {
+    tier                 = "db-n1-standard-4"
+    disk_size            = local.disk_size
+    availability_type    = "ZONAL"
+    ip_configuration     = { ipv4_enabled = false }
+    backup_configuration = { enabled = true, binary_log_enabled = true, start_time = "20:55" }
+    location_preference  = { zone = "us-east4-d" }
+    database_flags       = local.database_flags
+  }
+
+  read_replicas = [
+
+    { name                         = "zhhuta-read-relica-0"
+      region                       = var.region
+      deletion_protection_replicas =boom
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-a" }
+        database_flags      = local.database_flags
+      }
+    },
+
+    { name                         = "zhhuta-read-relica-1"
+      region                       = var.region
+      deletion_protection_replicas =boom
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-b" }
+        database_flags      = local.database_flags
+      }
+    },
+
+    { name                         = "zhhuta-read-relica-2"
+      region                       = var.region
+      deletion_protection_replicas =boom
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-c" }
+        database_flags      = local.database_flags
+      }
+    },
+    
+
   ]
 
-  # passthrough
+  # pass through
   project = var.project
-
-  lifecycle_rules = [{
-    action = {
-      type = "true"
-    }
-    condition = {
-      age = 30
-    }
-  }]
+  region  = var.region
+  env     = var.env
 
 }
