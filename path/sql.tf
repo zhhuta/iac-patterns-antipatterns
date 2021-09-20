af5b3e60f5a496ea8ba920f20ffd3acd5153d39e
Malek Hakim <hakimelek@gmail.com>
2021-09-20 13:23:59 -0400

    hello world

--- path/sql.tf
+++ path/sql.tf
@@ -38,7 +38,7 @@ module "Hello World" {
         disk_size           = local.disk_size
         availability_type   = "ZONAL"
         ip_configuration    = { ipv4_enabled = false }
-        location_preference = { zone = "us-east4-c" }
+        location_preference = { zone = "us-east4-b" }
         database_flags      = local.database_flags
       }
     },
