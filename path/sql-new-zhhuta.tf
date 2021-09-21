e5b4850af80fb5b66d8691d8e9687c1f96baf4ec
Vitaliy Zhhuta <zhhuta@gmail.com>
2021-09-21 18:26:02 +0300

    zhhuta test

---
+++ path/sql-new-zhhuta.tf
@@ -0,0 +1,38 @@
+locals {
+
+  database_flags = [
+    { name = "local_infile", value = "off" },
+    { name = "log_bin_trust_function_creators", value = "on" },
+  ]
+  disk_size = 50
+}
+
+module "zhhuta-test" {
+
+
+  source = "git::https://github.com/pcln/terraform-gcp-cloudsql.git//modules/mysql"
+
+  name                = "zhhuta-test-master"
+  database_version    = "MYSQL_8_0"
+  team                = "data_eng"
+  deletion_protection = true
+
+
+  settings = {
+    tier                 = "db-n1-standard-4"
+    disk_size            = local.disk_size
+    availability_type    = "ZONAL"
+    ip_configuration     = { ipv4_enabled = false }
+    backup_configuration = { enabled = true, binary_log_enabled = true, start_time = "20:55" }
+    location_preference  = { zone = "us-east1-a" }
+    database_flags       = local.database_flags
+  }
+
+  ]
+
+  # pass through
+  project = var.project
+  region  = var.region
+  env     = var.env
+
+}
