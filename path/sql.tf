74548209f9f3fc14d531225daf783d67c1f41d13
Malek Hakim <hakimelek@gmail.com>
2021-09-21 11:31:46 -0400

    hello world

--- path/sql.tf
+++ path/sql.tf
@@ -38,20 +38,7 @@ module "Hello World" {
         disk_size           = local.disk_size
         availability_type   = "ZONAL"
         ip_configuration    = { ipv4_enabled = false }
-        location_preference = { zone = "us-east4-a" }
-        database_flags      = local.database_flags
-      }
-    },
-
-    { name                         = "Hello World-read-relica-1"
-      region                       = var.region
-      deletion_protection_replicas = true
-      settings = {
-        tier                = "db-n1-standard-4"
-        disk_size           = local.disk_size
-        availability_type   = "ZONAL"
-        ip_configuration    = { ipv4_enabled = false }
-        location_preference = { zone = "us-east4-c" }
+        location_preference = { zone = "us-east4-b" }
         database_flags      = local.database_flags
       }
     },
