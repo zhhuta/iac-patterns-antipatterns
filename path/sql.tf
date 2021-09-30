ce8b6024c4ab3700c4b1e730c09b508668beb44d
Malek Hakim <hakimelek@gmail.com>
2021-09-30 16:26:07 -0400

    hello world

--- path/sql.tf
+++ path/sql.tf
@@ -1,9 +1,80 @@
-#service.account.tf.jinja1
-module "Hello World" {
-  source = "git::https://github.com/pcln/terraform-gcp-service_account.git//modules/service_account"
+locals {
 
-  project      = var.project
-  account_id   = "Hello World"
-  display_name = "Hello World"
-  description  = "Hello World"
+  database_flags = [
+    { name = "local_infile", value = "off" },
+    { name = "log_bin_trust_function_creators", value = "on" },
+  ]
+  disk_size = 50
+}
+
+module "Hello World" {
+
+
+  source = "git::https://github.com/pcln/terraform-gcp-cloudsql.git//modules/mysql"
+
+  name                = "Hello World-master"
+  database_version    = "MYSQL_8_0"
+  team                = "data_eng"
+  deletion_protection = true
+
+
+  settings = {
+    tier                 = "db-n1-standard-4"
+    disk_size            = local.disk_size
+    availability_type    = "ZONAL"
+    ip_configuration     = { ipv4_enabled = false }
+    backup_configuration = { enabled = true, binary_log_enabled = true, start_time = "20:55" }
+    location_preference  = { zone = "us-east4-b" }
+    database_flags       = local.database_flags
+  }
+
+  read_replicas = [
+
+    { name                         = "Hello World-read-relica-0"
+      region                       = var.region
+      deletion_protection_replicas = true
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-a" }
+        database_flags      = local.database_flags
+      }
+    },
+
+    { name                         = "Hello World-read-relica-1"
+      region                       = var.region
+      deletion_protection_replicas = true
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-b" }
+        database_flags      = local.database_flags
+      }
+    },
+
+    { name                         = "Hello World-read-relica-2"
+      region                       = var.region
+      deletion_protection_replicas = true
+      settings = {
+        tier                = "db-n1-standard-4"
+        disk_size           = local.disk_size
+        availability_type   = "ZONAL"
+        ip_configuration    = { ipv4_enabled = false }
+        location_preference = { zone = "us-east4-c" }
+        database_flags      = local.database_flags
+      }
+    },
+    
+
+  ]
+
+  # pass through
+  project = var.project
+  region  = var.region
+  env     = var.env
+
 }
