4729fda7bc5599bfabfd757e30e77620f6a207dd
Vitaliy Zhhuta <zhhuta@gmail.com>
2021-10-11 20:54:42 +0300

    zhhuta test commit

---
+++ path/sa-new1.tf
@@ -0,0 +1,9 @@
+#service.account.tf.jinja1
+module "zhhuta-sa" {
+  source = "git::https://github.com/pcln/terraform-gcp-service_account.git//modules/service_account"
+
+  project      = var.project
+  account_id   = "zhhuta-sa"
+  display_name = "zhhuta-sa"
+  description  = "New SA for zhhuta"
+}
