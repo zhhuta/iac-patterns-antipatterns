145928e92e8a00a184b5f5c65abee3142575b5f4
root <root@1d162e4f4443>
2021-12-09 17:56:15 +0000

    zhhuta commit

---
+++ path1/log-sink.tf
@@ -0,0 +1,12 @@
+module "zhhuta" {
+    source                 = "../terraform/modules/terraform-gcp-log-sink"
+    name                   = "zhhuta"
+    scope                  = "my"
+    project                = project-a
+    organization           = 12345678
+    destination            = "pubsub.googleapis.com/projects/project-a/topics/instance-activity"
+    # destination            = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
+    destination            = "bigquery.googleapis.com/projects/project-a/datasets/boom"
+    #filter                 = "resource.type = gce_instance AND severity >= WARN" #"resource.type = gce_instance AND severity >= WARN"
+    unique_writer_identity = true
+}
